da033dc58720827e1482058e98fad5df6a23af83
John DeMastri <demastri@msoe.edu>
2023-10-22 22:44:03 -0500

    Chapter 26 - p287 after listing 26.5 (last commit was listing 26.4...) - introduced a Column composable to start building a "toppings" list

--- app/src/main/java/edu/msoe/demastri/codapizza/MainActivity.kt
+++ app/src/main/java/edu/msoe/demastri/codapizza/MainActivity.kt
@@ -3,13 +3,22 @@ package edu.msoe.demastri.codapizza
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello World!")
+            Column {
+                Text(
+                    text = "Pineapple"
+                )
+
+                Text(
+                    text = "Whole pizza"
+                )
+            }
         }
     }
 }
