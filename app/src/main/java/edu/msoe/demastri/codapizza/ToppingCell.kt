e9a33d21ca4ff5d0c3f47ce3f75466900a188288
John DeMastri <demastri@msoe.edu>
2023-10-23 10:28:20 -0500

    Chapter 26 - p294 after listing 26.22 - ordering in modifier chains matters - padding before background

--- app/src/main/java/edu/msoe/demastri/codapizza/ToppingCell.kt
+++ app/src/main/java/edu/msoe/demastri/codapizza/ToppingCell.kt
@@ -1,14 +1,19 @@
 package edu.msoe.demastri.codapizza
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Checkbox
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import edu.msoe.demastri.codapizza.model.Topping
 import edu.msoe.demastri.codapizza.model.ToppingPlacement
 
@@ -39,7 +44,10 @@ public fun ToppingCell(
     onClickTopping: () -> Unit
 ) {
     Row(
-        verticalAlignment = Alignment.CenterVertically
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 16.dp)
+            .background(Color.Cyan)
     ) {
 
         Checkbox(
