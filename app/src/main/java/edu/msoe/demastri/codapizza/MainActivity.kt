284eb48be5b66c1a2e8347939dea04bb7bc4a0ff
John DeMastri <demastri@msoe.edu>
2023-11-17 07:18:17 -0600

    Chapter 29 - p326 after listing 29.12 - add theme and wrap screen in theme - button is now red

--- app/src/main/java/edu/msoe/demastri/codapizza/MainActivity.kt
+++ app/src/main/java/edu/msoe/demastri/codapizza/MainActivity.kt
@@ -11,7 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PizzaBuilderScreen()
+            AppTheme {
+                PizzaBuilderScreen()
+            }
         }
     }
 }
