74363eebeef98c84119dbe9f5bba20adad7df449
John DeMastri <demastri@msoe.edu>
2023-11-17 07:09:04 -0600

    Chapter 29 - p323 after listing 29.7 - change content scale for toppings when on 1/2

--- app/src/main/java/edu/msoe/demastri/codapizza/ui/PizzaHeroImage.kt
+++ app/src/main/java/edu/msoe/demastri/codapizza/ui/PizzaHeroImage.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,6 +49,7 @@ fun PizzaHeroImage(
             Image(
                 painter = painterResource(topping.pizzaOverlayImage),
                 contentDescription = null,
+                contentScale = ContentScale.Crop,
                 modifier = Modifier.focusable(false)
                     .aspectRatio(when (placement) {
                         ToppingPlacement.Left, ToppingPlacement.Right -> 0.5f
