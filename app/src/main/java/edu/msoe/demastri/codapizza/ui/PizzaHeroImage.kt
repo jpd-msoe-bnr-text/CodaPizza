c59c5165d4cd841759049d1db613041f0fc57c97
John DeMastri <demastri@msoe.edu>
2023-11-17 07:08:00 -0600

    Chapter 29 - p323 after listing 29.6 - change aspect ratio for toppings when on 1/2

--- app/src/main/java/edu/msoe/demastri/codapizza/ui/PizzaHeroImage.kt
+++ app/src/main/java/edu/msoe/demastri/codapizza/ui/PizzaHeroImage.kt
@@ -3,6 +3,8 @@ package edu.msoe.demastri.codapizza.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -34,10 +36,12 @@ fun PizzaHeroImage(
 ) {
     Box(
         modifier = modifier
+            .aspectRatio(1f)
     ) {
         Image(
             painter = painterResource(R.drawable.pizza_crust),
             contentDescription = stringResource(R.string.pizza_preview),
+            modifier = Modifier.fillMaxSize()
         )
 
         pizza.toppings.forEach { (topping, placement) ->
@@ -45,6 +49,10 @@ fun PizzaHeroImage(
                 painter = painterResource(topping.pizzaOverlayImage),
                 contentDescription = null,
                 modifier = Modifier.focusable(false)
+                    .aspectRatio(when (placement) {
+                        ToppingPlacement.Left, ToppingPlacement.Right -> 0.5f
+                        ToppingPlacement.All -> 1.0f
+                    })
             )
         }
     }
