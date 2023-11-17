4ccc85d8f60dd9c4edef389e4ad17e2c71275cbd
John DeMastri <demastri@msoe.edu>
2023-11-17 07:40:40 -0600

    Chapter 29 - p331 after listing 29.21 - remove appcompat from build environment

--- app/src/main/java/edu/msoe/demastri/codapizza/MainActivity.kt
+++ app/src/main/java/edu/msoe/demastri/codapizza/MainActivity.kt
@@ -1,13 +1,13 @@
 package edu.msoe.demastri.codapizza
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import edu.msoe.demastri.codapizza.model.Topping
 import edu.msoe.demastri.codapizza.model.ToppingPlacement
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
