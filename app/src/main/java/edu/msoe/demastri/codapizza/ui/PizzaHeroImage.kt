f49fdba807b2eca17493abc96f43adba2cb3e0a2
John DeMastri <demastri@msoe.edu>
2023-11-17 07:01:43 -0600

    Chapter 29 - p21 after listing 29.3 - add drawables and resources and added image to toppings definition

---
+++ app/src/main/java/edu/msoe/demastri/codapizza/ui/PizzaHeroImage.kt
@@ -0,0 +1,37 @@
+package edu.msoe.demastri.codapizza.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import edu.msoe.demastri.codapizza.R
+import edu.msoe.demastri.codapizza.model.Pizza
+import edu.msoe.demastri.codapizza.model.Topping
+import edu.msoe.demastri.codapizza.model.ToppingPlacement
+
+@Preview
+@Composable
+private fun PizzaHeroImagePreview() {
+    PizzaHeroImage(
+        pizza = Pizza(
+            toppings = mapOf(
+                Topping.Pineapple to ToppingPlacement.All,
+                Topping.Pepperoni to ToppingPlacement.Left,
+                Topping.Basil to ToppingPlacement.Right
+            )
+        )
+    )
+}
+
+@Composable
+fun PizzaHeroImage(
+    pizza: Pizza,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = painterResource(R.drawable.pizza_crust),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
